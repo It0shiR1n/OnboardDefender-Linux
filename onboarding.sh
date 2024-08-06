d2ce19675c41ef7a1994c2114f37453d3fdd18a4
Itoshi Rin <86209431+It0shiR1n@users.noreply.github.com>
2024-08-06 10:13:51 -0300

    Update onboarding.sh

--- onboarding.sh
+++ onboarding.sh
@@ -1,50 +1,24 @@
-#!/bin/bash
+#!/bin/bash 
 
 if [ "$(whoami)" == "root" ]; then
-    if [ -z "$1" ] || [ -z "$2" ]; then
-        echo "WARNING, ONLY WORKS IF THE DISTRO IS DEBIAN BASED"
-        echo "bash onboarding.sh debian|ubuntu onboardingScriptName.py"
-    else
-        sudo apt update
-        sudo apt upgrade -y
+    if [ "$1" == "" ]; then 
+       echo "Please pass the .py which can find inside the security.microsoft.com"
+   
+    else 
+       sudo apt update
+       sudo apt install curl libplist-utils gpg apt-transport-https -y
 
-        sudo apt install curl libplist-utils gpg apt-transport-https -y
+       wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb
+       dpkg -i packages-microsoft-prod.deb
+       rm -rf packages-microsoft-prod.deb
 
-        if [ "$1" == "debian" ] || [ "$1" == "ubuntu" ]; then
-            version=$(grep "VERSION_ID" /etc/os-release | cut -d "=" -f 2 | tr -d '"')
+       sudo apt update
+       sudo apt install mdatp -y 
+       mdatp connectivity test
+       python3 $1
 
-            curl -o microsoft.list "https://packages.microsoft.com/config/ubuntu/$version/prod.list"
-
-            if [ $? -ne 0 ]; then
-                echo "Probably your version isn't available in the Microsoft repository"
-                exit 1
-            fi
-
-            sudo mv microsoft.list /etc/apt/sources.list.d/microsoft-prod.list
-            curl -sSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-
-            sudo apt update
-            sudo apt install mdatp -y
-
-            clear
-
-            mdatp connectivity test
-            python3 "$2"
-
-            if [ "$(mdatp health | cut -d ":" -f 2)" == "true" ]; then
-                echo "Onboarded, it can take an hour to appear inside the security.microsoft.com"
-                exit 0
-            else
-                echo "Something went wrong"
-                exit 1
-            fi
-
-        else
-            echo "Distro unavailable"
-            exit 1
-        fi
-    fi
+    fi 
 else
-    echo "Please execute as root"
-    exit 1
+    echo "use root user"
+
 fi
